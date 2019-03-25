e594987424c0f0d90d6d15d6f3ebb821f1bba390
RaquelCC <rcanalesconcha@gmail.com>
2019-03-25 17:56:22 -0300

    agregando iconos a seccion de herramientas

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Tools.css';
 
-function Tools(props){
+function Tools(props) {
     return (
         <div className="tools-container">
-            
+            <div className="icons-container">
+                <i class="devicon-css3-plain"></i>
+                <i class="devicon-html5-plain"></i>
+                <i class="devicon-javascript-plain"></i>
+                <i class="devicon-react-original"></i>
+                <i class="devicon-nodejs-plain"></i>
+                <i class="devicon-slack-plain"></i>
+                <i class="devicon-visualstudio-plain"></i>
+                <i class="devicon-git-plain"></i>
+                <i className="devicon-github-plain"></i>
+            </div>
         </div>
     )
 }
